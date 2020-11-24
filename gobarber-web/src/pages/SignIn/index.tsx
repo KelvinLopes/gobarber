ba3be5a6abe5026799ca57aedf1d737515f91966
Kelvin Lopes <kelvin7android@gmail.com>
2020-11-24 17:45:58 -0300

    Function Add and Remove Toasts and add new version uuid

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -50,7 +50,11 @@ const SignIn: React.FC = () => {
           const errors = getValidationsErrors(err);
           formRef.current?.setErrors(errors);
         }
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Error na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
       }
     },
     [signIn, addToast],
