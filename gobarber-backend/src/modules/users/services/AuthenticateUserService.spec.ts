72d413cef29bc191770986be22f12fc845f8090d
KelvinLopes <kelvin7android@gmail.com>
2020-12-26 19:09:10 -0300

    AuthenticateUserService.spec.ts updated

--- gobarber-backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ gobarber-backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeUsersRepository
 from '../repositories/Fakes/FakeUserRepository';
 
@@ -37,6 +39,47 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
 
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const AuthenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(AuthenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const AuthenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(AuthenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: 'wrong-password',
+    })).rejects.toBeInstanceOf(AppError);
   });
 });
