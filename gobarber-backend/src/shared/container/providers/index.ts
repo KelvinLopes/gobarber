e10a6a9533035b69a42aa01ddf85045a14d2d8c5
KelvinLopes <kelvin7android@gmail.com>
2021-01-09 23:04:39 -0300

    Updateds and add nodemailer

--- gobarber-backend/src/shared/container/providers/index.ts
+++ gobarber-backend/src/shared/container/providers/index.ts
@@ -5,17 +5,29 @@ from './StorageProvider/implementations/DiskStorageProvider';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
+
 import EtherealMailProvider
 from './MailProvider/implementations/EtherealMailProvider';
 
-import IMailProvider from './MailProvider/models/IMailProvider';
+import IMailTemplateProvider
+from './MailTemplateProvider/models/IMailTemplateProvider';
+
+import HandlebarsMailTemplateProvider
+from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
