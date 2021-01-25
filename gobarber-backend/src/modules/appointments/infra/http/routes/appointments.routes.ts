3f811dd86e5390ae647e57ff4c5d23a3f2a09fd9
KelvinLopes <kelvin7android@gmail.com>
2021-01-25 19:30:42 -0300

    Updates

--- gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -5,12 +5,18 @@ from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import AppointmentsCrontroller from '../controllers/AppointmentsController';
 
+import ProviderAppointmentsController
+from '../controllers/ProviderAppointmentsController';
+
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsCrontroller();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/',appointmentsController.create);
+appointmentsRouter.get('/me',providerAppointmentsController.index);
+
 
 export default appointmentsRouter;
---
+++ gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,52 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository
+from '../repositories/fakes/FakeAppointmentsRepository';
+
+import ListProviderAppointmentsService
+from './ListProviderAppointmentsService';
+
+let fakeAppintementsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppintementsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppintementsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointmentOne = await fakeAppintementsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2021, 0, 20, 14, 0, 0),
+    });
+
+    const appointmentTwo = await fakeAppintementsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2021, 0, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() =>{
+     return new Date(2021, 0, 20, 11).getTime();
+    });
+
+
+    const availability = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      year: 2021,
+      month: 1,
+      day: 20,
+    });
+
+    expect(availability).toEqual(
+      [
+        appointmentOne,
+        appointmentTwo,
+      ],
+    );
+  });
+});
