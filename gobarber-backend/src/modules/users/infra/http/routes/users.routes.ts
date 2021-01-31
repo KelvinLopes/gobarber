77e93f11ecc6bc2b444a2c0b8f4dbb584285f291
KelvinLopes <kelvin7android@gmail.com>
2021-01-31 14:05:36 -0300

    Updated validations routes

--- gobarber-backend/src/modules/users/infra/http/routes/users.routes.ts
+++ gobarber-backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
@@ -14,7 +15,14 @@ const usersAvatarController = new UsersAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post('/',
+celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  },
+}),usersController.create);
 
 
 usersRouter.patch('/avatar', ensureAuthenticated,
